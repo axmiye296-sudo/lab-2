3bf9dfde4ea257aee3eea1489713dd2c6386dbbb
axmiye296-sudo <axmiye296@gmail.com>
2025-08-20 16:33:07 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Amy and Kate
+// Description: Hello World in C++
+//============================================================
+
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+int main()
+{
+  string name1="Amy";
+  hello1Function(name1);
+  string name2="Kate";
+  hello2Function(name2);
+  return 0;
+}
